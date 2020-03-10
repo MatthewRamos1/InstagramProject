19f71646a06183fc81f3d86a45a6cfd8d027319f
Matthew Ramos <mmramos309@gmail.com>
2020-03-10 17:06:26 -0400

    Set up image controller functions in profile vc to add actions for camera and gallery, temporarily saves chosen image to image view alone

---
+++ InstagramProject/Controllers/ProfileViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ProfileViewController.swift
+//  InstagramProject
+//
+//  Created by Matthew Ramos on 3/10/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    private var imagePickerController = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePickerController.delegate = self
+        
+    }
+    
+    @IBAction func addProfilePicturePressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { [weak self] alertAction in
+            self?.showImageController(isCameraSelected: true)
+        }
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { [weak self] alertAction in
+            self?.showImageController(isCameraSelected: false)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alertController.addAction(cameraAction)
+        }
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
+    private func showImageController(isCameraSelected: Bool) {
+        imagePickerController.sourceType = .photoLibrary
+        if isCameraSelected {
+            imagePickerController.sourceType = .camera
+        }
+        present(imagePickerController, animated: true)
+    }
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+        profileImageView.image = image
+        
+        
+    }
+    
+}
+
